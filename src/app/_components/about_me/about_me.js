86d61d049aba1e879b9d21d9c1683f4ef8955560
Luis Modesto <luisgmodesto12@gmail.com>
2025-06-08 00:11:59 -0300

    refactor: update about me text

--- src/app/_components/about_me/about_me.js
+++ src/app/_components/about_me/about_me.js
@@ -10,7 +10,7 @@ export default function AboutMe() {
       <div>
         <div className={styles.aboutme_box}>
           <TitleAnimation name="Sobre mim" />
-          <motion.p
+          <motion.div
             className="desc"
             initial={{
               top: "10px",
@@ -28,12 +28,34 @@ export default function AboutMe() {
               ease: "easeOut",
             }}
           >
-            Meu nome é Luis Felipe, tenho 20 anos e moro em Marília, São Paulo.
-            Sou uma pessoa criativa, persistente e altruísta. Além de programar,
-            sou apaixonado por música e tenho um gosto bastante eclético —
-            minhas playlists variam entre rock, blues e até mesmo música
-            clássica.
-          </motion.p>
+            <p>
+              Olá! Eu sou o Luis Felipe, um Desenvolvedor Front-End Pleno com 3
+              anos de experiência apaixonado por transformar ideias em
+              interfaces digitais intuitivas e de alta performance. Minha
+              jornada na programação começou com o desejo de construir soluções
+              que realmente façam a diferença, e desde então tenho me
+              aprofundado em tecnologias como React, Next.js, TypeScript e
+              GraphQL.
+            </p>
+            <p>
+              Sou um eterno aprendiz e sempre busco novos desafios. Embora meu
+              foco principal seja Front-End, tenho um grande interesse em
+              expandir minhas habilidades para o universo Fullstack e Mobile
+              (especialmente com Flutter), buscando sempre as melhores práticas
+              e soluções escaláveis.
+            </p>
+            <p>
+              Quando não estou codando, gosto de ouvir música e jogar videogame.
+              Acredito que o melhor código nasce da curiosidade, da colaboração
+              e de uma boa pausa no tempo livre para recarregar as energias!
+            </p>
+            <p>
+              Se você busca um desenvolvedor proativo, que assume
+              responsabilidades e adora ver o impacto real do seu trabalho,
+              ficarei feliz em conectar e conversar sobre como posso contribuir
+              com seus projetos!
+            </p>
+          </motion.div>
 
           <motion.a
             className="button"
@@ -59,7 +81,7 @@ export default function AboutMe() {
             Baixar CV
           </motion.a>
         </div>
-        <Image src="/300x300.png" width={286} height={261} alt=""></Image>
+        <Image src="/sobre_mim.png" width={384} height={256} alt=""></Image>
       </div>
     </section>
   );
