33ce56b124cb356b46d8dbe440f08978265d9e80
Luis Modesto <luisgmodesto12@gmail.com>
2025-06-08 00:14:09 -0300

    refactor: reorder projects

--- src/app/_components/projects/projects.js
+++ src/app/_components/projects/projects.js
@@ -3,62 +3,9 @@ import styles from "./projects.module.css";
 import EmblaCarousel from "../embla_carousel";
 const OPTIONS = { dragFree: true, loop: true, align: "start" };
 const SLIDES = [
-  {
-    title: "ChronosWork",
-    desc: `Registro e gestão de atividades de colaboradores. Os cadastros de novas atividades são enviados diretamente para uma planilha, utilizando a Google Sheets API.`,
-    img_url: "/slides/chronos-work.png",
-    links: {
-      github: "https://github.com/LuisFelipeMod/ChronosWork",
-      site: "https://chronos-work.vercel.app/",
-    },
-    stack: [
-      <svg viewBox="0 0 128 128">
-        <circle cx="64" cy="64" r="64"></circle>
-        <path
-          fill="url(#a)"
-          d="M106.317 112.014 49.167 38.4H38.4v51.179h8.614v-40.24l52.54 67.884a64.216 64.216 0 0 0 6.763-5.209z"
-        ></path>
-        <path fill="url(#b)" d="M81.778 38.4h8.533v51.2h-8.533z"></path>
-        <defs>
-          <linearGradient
-            id="a"
-            x1="109"
-            x2="144.5"
-            y1="116.5"
-            y2="160.5"
-            gradientTransform="scale(.71111)"
-            gradientUnits="userSpaceOnUse"
-          >
-            <stop stopColor="#fff"></stop>
-            <stop offset="1" stopColor="#fff" stopOpacity="0"></stop>
-          </linearGradient>
-          <linearGradient
-            id="b"
-            x1="121"
-            x2="120.799"
-            y1="54"
-            y2="106.875"
-            gradientTransform="scale(.71111)"
-            gradientUnits="userSpaceOnUse"
-          >
-            <stop stopColor="#fff"></stop>
-            <stop offset="1" stopColor="#fff" stopOpacity="0"></stop>
-          </linearGradient>
-        </defs>
-      </svg>,
-      <svg viewBox="0 0 128 128">
-        <path fill="#fff" d="M22.67 47h99.67v73.67H22.67z"></path>
-        <path
-          data-name="original"
-          fill="#007acc"
-          d="M1.5 63.91v62.5h125v-125H1.5zm100.73-5a15.56 15.56 0 017.82 4.5 20.58 20.58 0 013 4c0 .16-5.4 3.81-8.69 5.85-.12.08-.6-.44-1.13-1.23a7.09 7.09 0 00-5.87-3.53c-3.79-.26-6.23 1.73-6.21 5a4.58 4.58 0 00.54 2.34c.83 1.73 2.38 2.76 7.24 4.86 8.95 3.85 12.78 6.39 15.16 10 2.66 4 3.25 10.46 1.45 15.24-2 5.2-6.9 8.73-13.83 9.9a38.32 38.32 0 01-9.52-.1 23 23 0 01-12.72-6.63c-1.15-1.27-3.39-4.58-3.25-4.82a9.34 9.34 0 011.15-.73L82 101l3.59-2.08.75 1.11a16.78 16.78 0 004.74 4.54c4 2.1 9.46 1.81 12.16-.62a5.43 5.43 0 00.69-6.92c-1-1.39-3-2.56-8.59-5-6.45-2.78-9.23-4.5-11.77-7.24a16.48 16.48 0 01-3.43-6.25 25 25 0 01-.22-8c1.33-6.23 6-10.58 12.82-11.87a31.66 31.66 0 019.49.26zm-29.34 5.24v5.12H56.66v46.23H45.15V69.26H28.88v-5a49.19 49.19 0 01.12-5.17C29.08 59 39 59 51 59h21.83z"
-        ></path>
-      </svg>,
-    ],
-  },
-  {
+    {
     title: "SuperSign Prototype",
-    desc: `Protótipo de uma plataforma de assinatura digital (full stack).`,
+    desc: `Protótipo de plataforma de assinatura digital que demonstra minha expertise em desenvolvimento front-end com Next.js 14 (App Router) e TypeScript. Explore como construí uma solução robusta com autenticação segura (Next Auth), gerenciamento de documentos e interfaces modernas e responsivas (Shadcn-UI, TailwindCSS), utilizando Prisma ORM para manipulação de dados.`,
     img_url: "/slides/super-sign.png",
     links: {
       github: "https://github.com/LuisFelipeMod/supersign-prototype",
@@ -110,6 +57,59 @@ const SLIDES = [
         <path
           fill="#2d3748"
           d="M66.457.014a6.308 6.308 0 0 0-5.812 3.028l-47.87 78.072a6.379 6.379 0 0 0 .048 6.748l23.568 37.186a6.387 6.387 0 0 0 7.22 2.683l68.012-20.407a6.37 6.37 0 0 0 3.96-8.765l-43.72-94.85A6.298 6.298 0 0 0 66.46.014Zm1.795 23.95a2.348 2.348 0 0 1 2.448 1.433l30.16 69.784a2.39 2.39 0 0 1-1.512 3.241l-46.996 14.024a2.39 2.39 0 0 1-3.024-2.76l16.83-83.812a2.353 2.353 0 0 1 2.099-1.91z"
+        ></path>
+      </svg>,
+    ],
+  },
+  {
+    title: "ChronosWork",
+    desc: `Projeto de gestão de atividades e colaboradores desenvolvido com Next.js, TypeScript, NextUI e Tailwind CSS. Criei uma plataforma intuitiva para registro de tasks e gerenciamento de tempo, com funcionalidades de relatórios detalhados (com gráficos) e exportação em PDF. Demonstração de expertise em construir aplicações front-end completas e escaláveis, com diferenciais de acesso de gestor e colaborador e integração com Google Sheets.`,
+    img_url: "/slides/chronos-work.png",
+    links: {
+      github: "https://github.com/LuisFelipeMod/ChronosWork",
+      site: "https://chronos-work.vercel.app/",
+    },
+    stack: [
+      <svg viewBox="0 0 128 128">
+        <circle cx="64" cy="64" r="64"></circle>
+        <path
+          fill="url(#a)"
+          d="M106.317 112.014 49.167 38.4H38.4v51.179h8.614v-40.24l52.54 67.884a64.216 64.216 0 0 0 6.763-5.209z"
+        ></path>
+        <path fill="url(#b)" d="M81.778 38.4h8.533v51.2h-8.533z"></path>
+        <defs>
+          <linearGradient
+            id="a"
+            x1="109"
+            x2="144.5"
+            y1="116.5"
+            y2="160.5"
+            gradientTransform="scale(.71111)"
+            gradientUnits="userSpaceOnUse"
+          >
+            <stop stopColor="#fff"></stop>
+            <stop offset="1" stopColor="#fff" stopOpacity="0"></stop>
+          </linearGradient>
+          <linearGradient
+            id="b"
+            x1="121"
+            x2="120.799"
+            y1="54"
+            y2="106.875"
+            gradientTransform="scale(.71111)"
+            gradientUnits="userSpaceOnUse"
+          >
+            <stop stopColor="#fff"></stop>
+            <stop offset="1" stopColor="#fff" stopOpacity="0"></stop>
+          </linearGradient>
+        </defs>
+      </svg>,
+      <svg viewBox="0 0 128 128">
+        <path fill="#fff" d="M22.67 47h99.67v73.67H22.67z"></path>
+        <path
+          data-name="original"
+          fill="#007acc"
+          d="M1.5 63.91v62.5h125v-125H1.5zm100.73-5a15.56 15.56 0 017.82 4.5 20.58 20.58 0 013 4c0 .16-5.4 3.81-8.69 5.85-.12.08-.6-.44-1.13-1.23a7.09 7.09 0 00-5.87-3.53c-3.79-.26-6.23 1.73-6.21 5a4.58 4.58 0 00.54 2.34c.83 1.73 2.38 2.76 7.24 4.86 8.95 3.85 12.78 6.39 15.16 10 2.66 4 3.25 10.46 1.45 15.24-2 5.2-6.9 8.73-13.83 9.9a38.32 38.32 0 01-9.52-.1 23 23 0 01-12.72-6.63c-1.15-1.27-3.39-4.58-3.25-4.82a9.34 9.34 0 011.15-.73L82 101l3.59-2.08.75 1.11a16.78 16.78 0 004.74 4.54c4 2.1 9.46 1.81 12.16-.62a5.43 5.43 0 00.69-6.92c-1-1.39-3-2.56-8.59-5-6.45-2.78-9.23-4.5-11.77-7.24a16.48 16.48 0 01-3.43-6.25 25 25 0 01-.22-8c1.33-6.23 6-10.58 12.82-11.87a31.66 31.66 0 019.49.26zm-29.34 5.24v5.12H56.66v46.23H45.15V69.26H28.88v-5a49.19 49.19 0 01.12-5.17C29.08 59 39 59 51 59h21.83z"
         ></path>
       </svg>,
     ],
@@ -288,7 +288,7 @@ const SLIDES = [
 
 export default function Projects() {
   return (
-    <section id="projects">
+    <section className={styles.projects} id="projects">
       <div>
         <TitleAnimation name="Projetos" />
         <EmblaCarousel slides={SLIDES} options={OPTIONS} />
